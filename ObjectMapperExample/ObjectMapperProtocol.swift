f988d383bc5bae36709bfd74da222a072dc2de99
Andrew Stec <andrewstec@gmail.com>
2016-03-20 13:53:40 -0700

    Installed ObjectMapper using Pod

---
+++ ObjectMapperExample/ObjectMapperProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ObjectMapperProtocol.swift
+//  ObjectMapperExample
+//
+//  Created by Andrew Stec on 2016-03-20.
+//  Copyright © 2016 Andrew Stec. All rights reserved.
+//
+
+import UIKit
+
+class ObjectMapperProtocol: NSObject {
+
+}
